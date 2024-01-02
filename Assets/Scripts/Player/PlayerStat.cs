2dc3ce41a85091289bfe30cf064cc8227e81ae5a
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-01-02 16:38:16 +0900

    Player Move And Stats

---
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerStats
+{
+    private float currentHealth;
+    private float maxHealth;
+    private float speed;
+
+    // 각 스탯의 초기값 설정
+    public float Health { 
+        get { return currentHealth; } 
+        set {
+            if (value < 0) currentHealth = 0;
+            else if (value > maxHealth) currentHealth = maxHealth;
+            else currentHealth = value; 
+        }
+    }
+
+    public float MaxHealth {
+        get { return maxHealth; }
+        private set { maxHealth = value; }
+    }
+
+    public float Attack { get; set; }
+    public float AttackSpeed { get; set; }
+
+    public float Speed {
+        get { return speed; } 
+        set {
+            if (value < 0) speed = 0;
+            else if (value > 10f) speed = 10f;
+            else speed = value;
+        }
+    }
+
+    public void InitializeStats() {
+        MaxHealth = 100;
+        Health = MaxHealth;
+        Attack = 1;
+        AttackSpeed = 1;
+        Speed = 5f;
+    }
+    
+}
