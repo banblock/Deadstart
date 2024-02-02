585dcce116646940d3764fe967425ecfed57da6f
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-02-02 16:59:24 +0900

    MeleeWeapon

---
+++ Assets/Scripts/Weapon/Weapon/MeleeWeapon/MeleeWeapon.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class MeleeWeapon : Weapon
+{
+    [SerializeField]
+    Collider2D hitBox;
+
+    float attackRange = 1.0f;
+
+
+    protected override void PerformAttack()
+    {
+        
+        // 공격 시행시 히트 박스 On
+
+        // 공격하는 애니메이션 출력
+
+
+    }
+
+    private void ActiveHitBox()
+    {
+        if (hitBox != null) {
+            
+        }
+    }
+
+
+    private void SetHitBoxSize(float size)
+    {
+        float colliderSize = size * 2f + 1f;
+       
+        CapsuleCollider2D capsuleCollider2D = hitBox.GetComponent<CapsuleCollider2D>();
+        capsuleCollider2D.size = new Vector2(colliderSize, 0);
+        hitBox.offset = new Vector2(size, 0);
+    }
+}
